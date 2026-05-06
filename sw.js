35884ee58245f1d8c658cab77d2f947d69756c7b
Tomsen3 <tom.jansen@singende-krankenhaeuser.de>
2026-05-06 13:42:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// sw.js - stabilisierte Version (v5.1)
+// sw.js - stabilisierte Version (v5.2)
 
-const CACHE_NAME = 'innenpause-v5-1';
+const CACHE_NAME = 'innenpause-v5-5';
 const CACHE_PREFIX = 'innenpause-';
 
 const SHELL = [
